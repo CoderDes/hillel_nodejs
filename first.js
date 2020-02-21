38fe35158240b678dbe64aed7480459552704a63
Eugene Zubkov <eugzubv@gmail.com>
2020-02-21 18:17:46 +0200

    install chalk, try to log files with color

--- first.js
+++ first.js
@@ -1,9 +1,57 @@
 const fs = require("fs");
-const path = require("path");
+const pathModule = require("path");
+const chalk = require("chalk");
+
+function checkFileExtension(file, requiredExtensions) {
+  // TODO: check if it a file
+
+  const fileExt = pathModule.extname(file);
+  let isFileWithTargetExt = false;
+
+  for (ext of requiredExtensions) {
+    if (ext === fileExt) {
+      isFileWithTargetExt = true;
+    }
+  }
+
+  return isFileWithTargetExt;
+}
+
+let colorCounter = 0;
+function defineColorToDraw(colors) {
+  if (colors.length - 1 === colorCounter) {
+    colorCounter = 0;
+  }
+
+  return colors[colorCounter++];
+}
+
+function drawFileName(file, colors) {
+  console.log(chalk[defineColorToDraw(colors)](file));
+}
 
 function first(args) {
-  console.log("ARGUMENTS", args);
   const { ext = [], colors = ["red", "green", "blue"], deep = 0, path } = args;
+  const absolutePathToDirectory = pathModule.resolve("/home/eugene", path);
+  // TODO: convert string to array EXT
+
+  fs.access(absolutePathToDirectory, err => {
+    if (err) {
+      throw new Error(err.message);
+    }
+
+    fs.readdir(absolutePathToDirectory, (err, files) => {
+      if (err) {
+        throw new Error(err.message);
+      }
+
+      files.forEach(file => {
+        if (checkFileExtension(file, ext)) {
+          drawFileName(file, colors);
+        }
+      });
+    });
+  });
 }
 
 module.exports = first;
