cbbdab801851067e08a90a0fc56a7cf43ef5f43b
Eugene Zubkov <eugzubv@gmail.com>
2020-02-22 12:20:42 +0200

    log target files with color

--- first.js
+++ first.js
@@ -2,8 +2,21 @@ const fs = require("fs");
 const pathModule = require("path");
 const chalk = require("chalk");
 
+function convertArgToArray(argument) {
+  if (Array.isArray(argument)) {
+    return argument;
+  }
+
+  return argument
+    .replace(/'/g, "")
+    .replace(/\[/g, "")
+    .replace(/\]/g, "")
+    .split(",")
+    .map(elem => elem.trim());
+}
+
 function checkFileExtension(file, requiredExtensions) {
-  // TODO: check if it a file
+  requiredExtensions = convertArgToArray(requiredExtensions);
 
   const fileExt = pathModule.extname(file);
   let isFileWithTargetExt = false;
@@ -19,21 +32,20 @@ function checkFileExtension(file, requiredExtensions) {
 
 let colorCounter = 0;
 function defineColorToDraw(colors) {
-  if (colors.length - 1 === colorCounter) {
+  if (colors.length === colorCounter) {
     colorCounter = 0;
   }
-
   return colors[colorCounter++];
 }
 
 function drawFileName(file, colors) {
+  colors = convertArgToArray(colors);
   console.log(chalk[defineColorToDraw(colors)](file));
 }
 
 function first(args) {
   const { ext = [], colors = ["red", "green", "blue"], deep = 0, path } = args;
-  const absolutePathToDirectory = pathModule.resolve("/home/eugene", path);
-  // TODO: convert string to array EXT
+  const absolutePathToDirectory = pathModule.resolve("/home/eugene", args.path);
 
   fs.access(absolutePathToDirectory, err => {
     if (err) {
