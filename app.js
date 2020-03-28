928327bd359fad77c5e48ad44f130361b47bab30
Eugene Zubkov <eugzubv@gmail.com>
2020-03-28 15:55:31 +0200

    refactor server in oop style

--- app.js
+++ app.js
@@ -6,118 +6,47 @@ const FileType = require("file-type");
 
 const { page, clientScriptPath } = require("./assets/pageString.js");
 const Logger = require("./util/Logger.js");
+const Router = require("./util/Router.js");
 
-const assetsPath = new RegExp("^/assets", "i");
+class Server {
+  #serverData;
 
-// TODO: handle error code in util function
-const logger = new Logger();
+  constructor(Router, Logger) {
+    this.server = http.createServer();
+    this.router = new Router();
+    this.logger = new Logger();
+  }
 
-async function router(servData) {
-  const { request, response } = servData;
-  const { method, url } = request;
-  const cleanUrl = new URL(url, `http://${request.headers.host}`);
-  try {
-    if (method === "GET") {
-      const { pathname } = cleanUrl;
+  initialize() {
+    this.server.on("listening", (request, response) => {
+      this.#serverData = {
+        request,
+        response,
+        server: this.server,
+        logger: this.logger
+      };
 
-      if (pathname === "/") {
-        console.log("PASS REGEXP IN ROOT");
-        response.setHeader("Content-type", "text/html");
-        response.statusCode = 200;
-        response.statusMessage = "Success";
-        response.write(page);
-        response.end();
-        return;
+      try {
+        this.router.initialize(this.#serverData);
+      } catch (err) {
+        throw new Error(err.message);
       }
-      if (assetsPath.test(pathname)) {
-        const filePath = join(__dirname, pathname);
+    });
 
-        accessSync(filePath);
+    this.server.once("listening", (request, response) => {
+      this.logger.observeInterval = 5 * 1000;
+      this.logger.initObserve(this.#serverData);
+    });
 
-        const rs = createReadStream(filePath);
+    this.server.on("close", () => {
+      this.logger.removeObserve();
+    });
 
-        rs.once("data", async chunk => {
-          const fileTypeData = await FileType.fromBuffer(chunk);
-          response.setHeader("Content-type", fileTypeData.mime);
-          response.statusCode = 200;
-          response.statusMessage = "Success";
-          response.write(chunk);
-          rs.pipe(response);
-        });
-
-        rs.on("error", err => {
-          response.statusCode = 404;
-          response.statusMessage = "Not found";
-          response.write("Error. Possibly, such file doesn't exist.");
-          response.end();
-        });
-
-        response.once("pipe", () => {
-          logger.logMode = "time";
-          logger.createLogFile(join(__dirname, "assets", "log-time.txt"));
-          logger.timer.startCount(new Date());
-        });
-
-        response.once("unpipe", () => {
-          logger.timer.endCount(new Date());
-          response.end();
-        });
-
-        response.on("close", () => {
-          logger.generateLogMessage(response);
-          logger.logMode = "observe";
-        });
-        return;
-      }
-      response.statusCode = 404;
-      response.statusMessage = "Not found";
-      response.write("Error");
-      response.end();
-      // TODO: logging here too
-    }
-    if (method === "POST") {
-    }
-    if (method === "PUT") {
-    }
-    if (method === "DELETE") {
-    }
-  } catch (err) {
-    response.statusCode = 500;
-    response.statusMessage = "Internal error";
-    response.write("Something went wrong. \n");
-    response.end(`Error: ${err.message}`);
+    this.server.listen(3000, "localhost", () => {
+      console.log("Server is running...");
+    });
   }
 }
 
-const server = http.createServer((request, response) => {
-  const serverData = {
-    request,
-    response,
-    server: server
-  };
-
-  try {
-    // TODO: maybe we don't need so much data any more here
-    router(serverData);
-  } catch (err) {
-    throw new Error(err.message);
-  }
-});
-
-server.once("listening", (request, response) => {
-  const serverData = {
-    request,
-    response,
-    server: server
-  };
-  logger.observeInterval = 5 * 1000;
-  logger.initObserve(serverData);
-});
-
-server.on("close", () => {
-  logger.removeObserve();
-});
-
-server.listen(3000, "localhost", () => {
-  console.log("SERVER IS RUNNING");
-});
+const server = new Server(Router, Logger);
+server.initialize();
--- util/Router.js
+++ util/Router.js
@@ -1,7 +1,107 @@
+const { createReadStream, accessSync, appendFileSync } = require("fs");
+const { join, resolve } = require("path");
+
+const FileType = require("file-type");
+
+const { page, clientScriptPath } = require("../assets/pageString.js");
+
 // TODO: move routing logic here
+const Logger = require("./Logger");
+
 class Router {
-  constructor(request, response) {
-    this.request = request;
-    this.response = response;
+  #assetsPathRegExp = new RegExp("^/assets", "i");
+
+  initialize(serverData) {
+    this.request = serverData.request;
+    this.response = serverData.response;
+    this.logger = serverData.logger;
+  }
+  handleMethod(request) {
+    try {
+      const { method } = request;
+      switch (method) {
+        case "GET":
+          this.handleGet(request);
+          break;
+        case "POST":
+          this.handlePost(request);
+          break;
+        case "PUT":
+          this.handlePut(request);
+          break;
+        case "DELETE":
+          this.handleDelete(request);
+          break;
+        default:
+          this.handleError();
+      }
+    } catch (err) {
+      this.handleError(err, "Something wrong with request method.");
+    }
+  }
+  handleGet(request) {
+    const parsedUrl = new URL(request.url, `http://${request.headers.host}`);
+    const { pathname } = parsedUrl;
+
+    if (pathname === "/") {
+      this.response.setHeader("Content-type", "text/html");
+      this.response.statusCode = 200;
+      this.response.statusMessage = "Success";
+      this.response.write(page);
+      this.response.end();
+      return;
+    }
+
+    if (this.#assetsPathRegExp.test(pathname)) {
+      const filePath = join(__dirname, pathname);
+
+      accessSync(filePath);
+
+      const rs = createReadStream(filePath);
+
+      rs.once("data", async chunk => {
+        const fileTypeData = await FileType.fromBuffer(chunk);
+        response.setHeader("Content-type", fileTypeData.mime);
+        response.statusCode = 200;
+        response.statusMessage = "Success";
+        response.write(chunk);
+        rs.pipe(response);
+      });
+
+      rs.on("error", err => {
+        this.handleError(err, "Error. Possibly, such file doesn't exist.");
+      });
+
+      this.response.once("pipe", () => {
+        logger.logMode = "time";
+        logger.createLogFile(join(__dirname, "assets", "log-time.txt"));
+        logger.timer.startCount(new Date());
+      });
+
+      this.response.once("unpipe", () => {
+        logger.timer.endCount(new Date());
+        response.end();
+      });
+
+      this.response.on("close", () => {
+        logger.generateLogMessage(response);
+        logger.logMode = "observe";
+      });
+      return;
+    }
+  }
+  handlePost(request) {}
+  handlePut(request) {}
+  handleDelete(request) {}
+  handleError(err, responseMessage) {
+    console.log("ERROR", err);
+    // TODO write real statuses to response
+    // this.response.statusCode = code;
+    // this.response.statusMessage = status;
+    // this.response.write(`${message}.\n`)
+    // this.response.end();
+    // throw new Error(`Error: ${code}. ${message}`);
   }
 }
+
+module.exports = Router;
