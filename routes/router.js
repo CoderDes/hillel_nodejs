e12952f6d8c86f3a84998c40a5d38a9e6bdb0969
Eugene <eugzubv@gmail.com>
2020-04-16 17:51:47 +0300

    finish logging

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,16 @@
 const { Router } = require("express");
 
 const { getAllFilesWithExtension } = require("../controller/files.js");
+const Logger = require("../util/Logger.js");
 
 const router = Router();
+const logger = new Logger();
+logger.startObserve();
+
+router.use((req, res, next) => {
+  logger.handleIncomingRequest(req);
+  next();
+});
 
 router.route("/").get((req, res, next) => {
   res.status(200).render("home.nj");
--- util/Logger.js
+++ util/Logger.js
@@ -5,12 +5,28 @@ class Logger {
   #logDirPath = join(__dirname, "..", "log");
   #timeLog = join(this.#logDirPath, "time-log.txt");
   #observeLog = join(this.#logDirPath, "observe-log.txt");
+  #requestCounter = 0;
+  #userAgents = new Set();
+
+  constructor() {
+    this.intervalId;
+    this.observeDelay = 1000 * 60;
+  }
 
   set setLogFileName(value = "log.txt") {
     if (typeof value !== "string") {
       throw new Error("Argument must be a string.");
     }
     this.#timeLog = join(__dirname, "..", value);
+  }
+  set setObserveDelay(value = 3000) {
+    if (typeof value !== "number") {
+      throw new Error("Value must be a number.");
+    }
+    if (value === Infinity || Number.isNaN(value)) {
+      throw new Error("Value must not be an Infinity or NaN");
+    }
+    this.observeDelay = value;
   }
   createLogDir() {
     return promises.mkdir(this.#logDirPath);
@@ -26,7 +42,24 @@ class Logger {
     const path = type === "observe" ? this.#timeLog : this.#observeLog;
     return promises.writeFile(path, "");
   }
-  writeObserveLogData() {}
+  writeObserveLogData() {
+    const content = `\n incoming requests: ${
+      this.#requestCounter
+    };\n unique user-agents: ${[...this.#userAgents]
+      .map(agent => agent)
+      .join(",\n")}.`;
+    promises.writeFile(this.#observeLog, content).catch(err => {
+      throw new Error(err);
+    });
+  }
+  startObserve() {
+    const observe = this.writeObserveLogData.bind(this);
+    this.intervalId = setInterval(observe, this.observeDelay);
+  }
+  handleIncomingRequest(req) {
+    this.#requestCounter++;
+    this.#userAgents.add(req.headers["user-agent"]);
+  }
   writeTimeLogData({ start, end, duration, status }) {
     const content = `\n startAt: ${start};\n endAt: ${end};\n duration: ${duration} sec;\n status: ${status};\n`;
     return promises.appendFile(this.#timeLog, content);
